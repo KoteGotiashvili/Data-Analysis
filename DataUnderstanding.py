51330f5d7dbc59659d5db0b7e90e78a723f1b570
KoteGotiashvili <kote.gotiashvili@gmail.com>
2024-07-24 22:46:58 +0400

    organize code better way

--- DataUnderstanding.py
+++ DataUnderstanding.py
@@ -3,23 +3,32 @@ from matplotlib.pylab import plt
 import numpy as np
 import pandas as pd
 
-pd.set_option('display.max_columns', 2)
-# read dataset
-df = pd.read_csv("./data/coaster_db.csv")
+def load_data(csv_source):
+    # display more columns
+    pd.set_option('display.max_columns', 20)
+    # read dataset
+    df = pd.read_csv(csv_source)
 
-#understand the dataset
+    return df
 
-#get shape row/column
-print(df.shape)
+def understand_basics(df):
+    # understand the dataset
 
-# get columns
-print(df.columns)
+    # get shape row/column
+    print(df.shape)
 
-# find datatypes
-print(df.dtypes)
+    # get columns
+    print(df.columns)
 
-# describe data
-print(df.describe())
+    # find datatypes
+    print(df.dtypes)
 
-# get columns info
-print(df.info())
+    # describe data
+    print(df.describe())
+
+    # get columns info
+    print(df.info())
+
+# get basic info about data
+# understand_basics(load_data('./data/coaster_db.csv'))
+
